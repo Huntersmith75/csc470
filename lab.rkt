0b18a9032ab63200bf28a59bd4319ee8b3061dcb
Huntersmith75 <91543936+Huntersmith75@users.noreply.github.com>
2021-11-16 10:07:50 -0500

    Add files via upload

---
+++ lab.rkt
@@ -0,0 +1,44 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-advanced-reader.ss" "lang")((modname lab) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
+(define block-exp '(1 a (2 b (3 c (4 d (5 e (6 f (7 g (8 h)))))))))
+(define block-num 1)
+(define block-symbol 'a)
+(define block-symbol-block '(a (2 b 7) (5 e 6)))
+
+
+
+(define count-block-exp
+  (lambda (block-exp)
+    (cond
+      ((null? block-exp) 0)
+      ((number? block-exp) block-exp)
+      ((symbol? block-exp) 0)
+      ((number? (car block-exp)) (+ (car block-exp)
+                                    (if (null? (cddr block-exp)) 0
+                                   (count-block-exp (caddr block-exp)))))
+      (else (+ (count-block-exp (cadr block-exp)) (count-block-exp (caddr block-exp)))))))
+
+(count-block-exp block-exp)
+(count-block-exp block-num)
+(count-block-exp block-symbol)
+(count-block-exp block-symbol-block)
+
+
+
+(define collect-symbols
+  (lambda (block-exp)
+    (cond
+      ((null? block-exp) '())
+      ((number? block-exp) '())
+      ((symbol? block-exp) block-exp)
+      ((number? (car block-exp)) (cons (cadr block-exp)
+                                       (if (null? (cddr block-exp)) '()
+                                       (collect-symbols (caddr block-exp)))))
+      (else (cons (car block-exp) (append (collect-symbols (cadr block-exp))
+                                        (collect-symbols (caddr block-exp))))))))
+
+(collect-symbols block-exp)
+(collect-symbols block-num)
+(collect-symbols block-symbol)
+(collect-symbols block-symbol-block)
