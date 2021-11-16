ec66461859bb58d670781b450e460f57faf90bcd
Huntersmith75 <91543936+Huntersmith75@users.noreply.github.com>
2021-11-16 08:56:43 -0500

    Update HW12.rkt

--- HW12.rkt
+++ HW12.rkt
@@ -1,9 +1,4 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-advanced-reader.ss" "lang")((modname HW12) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-advanced-reader.ss" "lang")((modname Parser) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+
 ; ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ; ~~~~~ HELPER FUNCTIONS ~~~~~
 ; ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
